f87bb5019cbe4cf5485c8c0652d8aa665cd9f78d
kamleshsilag <kamleshsilag42@gmail.com>
2020-06-29 18:09:53 +0530

    Course API

--- src/main/java/org/example/springbootstarter/course/Course.java
+++ src/main/java/org/example/springbootstarter/course/Course.java
@@ -1,24 +1,31 @@
-package org.example.springbootstarter.topic;
+package org.example.springbootstarter.course;
+
+import org.example.springbootstarter.topic.Topic;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
-public class Topic {
+public class Course {
 
     @Id
     private String id;
     private String name;
     private String description;
 
-    public Topic(){
+    @ManyToOne
+    private Topic topic;
+
+    public Course(){
 
     }
 
-    public Topic(String id, String name, String description) {
+    public Course(String id, String name, String description, String topicId) {
         this.id = id;
         this.name = name;
         this.description = description;
+        this.topic = new Topic(topicId,"","");
     }
 
 
@@ -46,5 +53,12 @@ public class Topic {
         this.description = description;
     }
 
+    public Topic getTopic() {
+        return topic;
+    }
+
+    public void setTopic(Topic topic) {
+        this.topic = topic;
+    }
 }
 
